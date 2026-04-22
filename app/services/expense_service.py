44c7e13c2662ba42e6666c8e7bc5ad81044d54b1
Ashish Sharma <ashu11vats@gmail.com>
2026-04-22 10:56:39 +0530

    Updated expense_service.py including service logic for create, update, delete, filtering and getting summary of all expenses

--- app/services/expense_service.py
+++ app/services/expense_service.py
@@ -42,4 +42,46 @@ class ExpenseService:
             "id": expense_id,    # Newly created expense ID
         }
     
+    # Service method for updating expense
+    def update_expense(self, expense_id, expense):
+        
+        # Call Repository method to update the expense in data storage (CSV/DB)
+        updated = self.repo.update_expense(expense_id, expense)
+        
+        # If update failed (expense not found)
+        if not updated:
+            
+            # Return error response (typically used in API layer)
+            return {"error": "Expense not found"}
+        
+        # If update successful -> return success message with ID
+        return {"message": "Expense updated", "id": expense_id}
     
+    # Service method for deleting expense
+    def delete_expense(self, expense_id):
+        
+        # Call repository method to delete expense
+        deleted = self.repo.delete_expense(expense_id)
+        
+        # If deletion failed (expense not found)
+        if not deleted:
+            
+            # Return error response (typically used in API layer)
+            return{"error": "Expense not found"}
+        
+        # If deletion successful -> return success message with ID
+        return {"message": "Expense deleted"}
+    
+    # Service method for filtering expenses
+    def filter_expenses(self, category = None, date = None, min_amount = None):
+        
+        # Directly delegate filtering logic to repository layer
+        # Repository handles actual filtering of data
+        return self.repo.filter_expenses(category, date, min_amount)
+    
+    # Service method for getting summary of all expenses
+    def get_summary(self):
+        
+        # Delegate summary/analytics calculation to repository
+        # Returns total spending, category-wise and monthly summaries
+        return self.repo.get_summary()
